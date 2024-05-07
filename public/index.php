bd97a0339bd1cccf80f48a0806bea5459ff1a0cb
kaufmajo <joachim_kaufmann@bluewin.ch>
2024-05-07 11:21:29 +0200

    ...

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
 // start custom bootstrap code
 // -------------------------
 
-const REFRESH_STATIC_FILES = '17';
+const REFRESH_STATIC_FILES = '18';
 
 function ddd(mixed $var): never
 {
