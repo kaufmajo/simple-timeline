6e441c3f1b00d6a541a4c7fb89c68307d5e43529
Joachim Kaufmann <joachim_kaufmann@bluewin.ch>
2024-07-18 23:30:30 +0200

    ...

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
 // start custom bootstrap code
 // -------------------------
 
-const REFRESH_STATIC_FILES = '18';
+const REFRESH_STATIC_FILES = '19';
 
 function ddd(mixed $var): never
 {
